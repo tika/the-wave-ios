f837ba7129d5f094f2aaef77f19dec72d8416483
tika <48658947+tika@users.noreply.github.com>
2024-11-09 11:11:44 -0500

    feat: add location of umass!

--- hackumass/ContentView.swift
+++ hackumass/ContentView.swift
@@ -9,9 +9,10 @@ import SwiftUI
 import MapboxMaps
 
 struct ContentView: View {
+    
     var body: some View {
-        let center = CLLocationCoordinate2D(latitude: 39.5, longitude: -98.0)
-        Map(initialViewport: .camera(center: center, zoom: 2, bearing: 0, pitch: 0))
+        let center = CLLocationCoordinate2D(latitude: 42.3909093, longitude: -72.6081182)
+        Map(initialViewport: .camera(center: center, zoom: 5, bearing: 0, pitch: 0))
             .ignoresSafeArea()
     }
 }
