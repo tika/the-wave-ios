dfccfceb9201ab67a2933858320e9089cafd10cf
tika <48658947+tika@users.noreply.github.com>
2024-11-09 13:31:45 -0500

    feat: add broken google maps

--- hackumass/ContentView.swift
+++ hackumass/ContentView.swift
@@ -6,17 +6,43 @@
 //
 
 import SwiftUI
-import MapboxMaps
+import GoogleMaps
 
-struct ContentView: View {
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> GMSMapView {
+        let options = GMSMapViewOptions()
+        options.camera = GMSCameraPosition.camera(
+            withLatitude: -33.86,
+            longitude: 151.20,
+            zoom: 6.0
+        )
+        
+        let mapView = GMSMapView(options: options)
+        
+        // Create and add marker
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+        return mapView
+    }
     
-    var body: some View {
-        let center = CLLocationCoordinate2D(latitude: 42.3909093, longitude: -72.6081182)
-        Map(initialViewport: .camera(center: center, zoom: 5, bearing: 0, pitch: 0))
-            .ignoresSafeArea()
+    func updateUIView(_ mapView: GMSMapView, context: Context) {
+        // Update the view if needed
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    var body: some View {
+        MapView()
+            .edgesIgnoringSafeArea(.all) // Make map full screen
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
